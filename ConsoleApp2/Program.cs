f0f1d828eda5da9f6e4878042f8d0070b2ba446b
admin <admin@LAPTOP-BKUCOEKT>
2024-09-10 10:14:29 +0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double r, h;
+
+                while (true)
+                {
+                    Console.Write("Nhap ban kinh : ");
+                    if (double.TryParse(Console.ReadLine(), out r) && r > 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Nhap sai roi , nhap lai!");
+                }
+
+                while (true)
+                {
+                    Console.Write("Nhap chieu cao : ");
+                    if (double.TryParse(Console.ReadLine(), out h) && h > 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Nhap sai roi , nhap lai!");
+                }
+
+                double area = Math.PI * r * r * h;
+                Console.WriteLine($"Dien tich hinh thang la : {area}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Loi: " + ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
